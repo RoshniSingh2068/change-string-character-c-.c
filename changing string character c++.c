9db3beb6a6f4fe62e7647e0fd4c97f0a905fdf32
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:05:35 +0530

    Create changing string character c++.c

---
+++ changing string character c++.c
@@ -0,0 +1,15 @@
+// changing the name of the string by index method
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string mystring = "Hello"; //define the string
+    mystring[0] = 'j'; // changing the letter by the index
+    cout<< mystring; // printing the string
+    return 0; // output
+
+
+}
